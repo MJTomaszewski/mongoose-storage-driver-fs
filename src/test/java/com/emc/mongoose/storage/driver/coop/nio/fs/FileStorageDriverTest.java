4e21a517cfd1002ba947ca7699d9038e0b7a73fa
MJTomaszewski <118823473+MJTomaszewski@users.noreply.github.com>
2023-01-09 13:26:05 +0100

    Update FileStorageDriverTest.java

--- src/test/java/com/emc/mongoose/storage/driver/coop/nio/fs/FileStorageDriverTest.java
+++ src/test/java/com/emc/mongoose/storage/driver/coop/nio/fs/FileStorageDriverTest.java
@@ -49,7 +49,7 @@ public class FileStorageDriverTest {
 	}
 
 	@Test
-	@Ignore // fails @ travis with unclear reason
+//	@Ignore // fails @ travis with unclear reason
 	public final void testList()
 					throws Exception {
 
